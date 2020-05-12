4351be93cd4651bc70c86e79bb9ecd5fb60251b8
guptaankit18598 <65112096+guptaankit18598@users.noreply.github.com>
2020-05-12 10:52:13 +0530

    added files

---
+++ animation.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+  $('li.fields').filter(':nth-child(n+4)').addClass('hide');
+  $('ul').on('click','li.Title',function() {
+  $(this).next().slideDown(200).siblings('li.fields').slideUp(200);
+})
+});
---
+++ validation.js
@@ -0,0 +1,18 @@
+﻿function validateform() {
+  for(var i=0;i<myform.elements.length;i++) {
+  if(myform.elements[i].className == "req" && myform.elements[i].value.length == 0) {
+  
+    alert('Please fill all the required fields');
+    return false;
+
+
+}
+}
+  var Email = document.getElementsById('Email').value;
+  var atpos = Email.indexOf('@');
+  var dotpos = Email.indexOf('.');
+  if(atpos<1 || dotpos<atpos+2 || dotpos+2 >= x.length) {
+  alert('Please fill the correct emailId');
+  return false; 
+}
+}
